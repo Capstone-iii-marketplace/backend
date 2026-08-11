0c4eab990358580302815c7fcb26b066b2db9ee0
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2026-08-11 12:17:31 -0400

    added skeleton folders/files

---
+++ models/Users.js
@@ -0,0 +1,62 @@
+const { DataTypes } = require("sequelize");
+const db = require("./db");
+
+const User = db.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    campus: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+
+    verifiedAt: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+  },
+  {
+    tableName: "users",
+    underscored: true,
+
+    defaultScope: {
+      attributes: {
+        exclude: ["passwordHash"],
+      },
+    },
+
+    hooks: {
+      beforeSave(user) {
+        if (user.email) {
+          user.email = user.email.toLowerCase().trim();
+        }
+      },
+    },
+  },
+);
+
+module.exports = User;
