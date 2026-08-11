f17090d3944c3c2de0bfec57f1a43a7ca11ef936
PhyoThihaOo <phyothihaoo.pto@gmail.com>
2026-08-11 16:24:22 -0400

    added CRUDroute for listings

--- routes/listing.route.js
+++ routes/listing.route.js
@@ -2,12 +2,29 @@ const express = require("express");
 const {
   getListings,
   getListingById,
+  createListing,
+  updateListing,
+  deleteListing,
 } = require("../controllers/listing.controller");
 const { requireAuth } = require("../middleware/auth.middleware");
+const {
+  loadOwnedListing,
+  requireActive,
+} = require("../middleware/listing.middleware");
 
 const router = express.Router();
 
 router.get("/", requireAuth, getListings);
+router.post("/", requireAuth, createListing);
+
 router.get("/:id", requireAuth, getListingById);
+router.patch(
+  "/:id",
+  requireAuth,
+  loadOwnedListing,
+  requireActive,
+  updateListing,
+);
+router.delete("/:id", requireAuth, loadOwnedListing, deleteListing);
 
 module.exports = router;
